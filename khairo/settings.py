019f8681a6d15dfed07fcad49b878a2187056028
Kokoserver <owonikokoolaluwa@gmail.com>
2021-02-26 13:29:43 +0100

    making changes to settings variables.

--- khairo/settings.py
+++ khairo/settings.py
@@ -13,7 +13,7 @@ DATABASE_URI = os.getenv("DATABASE_URL")
 ACCESS_TOKEN_EXPIRE_TIME = os.getenv("ACCESS_TOKEN_EXPIRE_TIME")
 REFRESH_TOKEN_EXPIRE_TIME = os.getenv("REFRESH_TOKEN_EXPIRE_TIME")
 # url in localhost        url in production
-WEBSITE_URL = "https://khairodiet.herokuapp.com"  # "http://127.0.0.1:8000"
+WEBSITE_URL = os.getenv("WEBSITE_URL")
 WEBSITE_NAME = "khairo"
 API_BASE_URI = "/api/v1"
 BASE_DIR = Path.cwd()
